cecd8135787491debb107ac96daec97075f6deb8
Leya-Olkh <128980142+Leya-Olkh@users.noreply.github.com>
2023-03-26 17:28:51 +0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,40 +1,25 @@
-﻿/***Задача 4:**
+﻿/***Задача 6:**
 
 **Условие:**
 
-Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Напишите программу, которая на вход принимает число и выдаёт,
+является ли число чётным (делится ли оно на два без остатка).
 
 **Например:**
 
-2, 3, 7 -> 7
-44 5 78 -> 78
-22 3 9 -> 22*/
+4 -> да
+-3 -> нет
+7 -> нет*/
 
 Console.Clear();
-Console.Write("a:");
-int a = int.Parse(Console.ReadLine());
-Console.Write("b:");
-int b = int.Parse(Console.ReadLine());
-Console.Write("c:");
-int c = int.Parse(Console.ReadLine());
-int max = 0;
-Console.WriteLine($"{a < b}");
-if(a > b)
-{
-    Console.WriteLine($"{max = a}");
-}else
-{
-	Console.WriteLine($"{max = b}");
-}
-Console.WriteLine($"{max > c}");
-if(max > c)
+Console.Write("Введите число:");
+int number = int.Parse(Console.ReadLine());
+if(number%2 != 0)
 {
     Console.Clear();
-    int sqr = max;
-    Console.WriteLine($"Максимальное число {sqr}");
+    Console.WriteLine($"Число {number} нечётное");
 }else
 {
     Console.Clear();
-    int sqr = c;
-    Console.WriteLine($"Максимальное число {sqr}");
+    Console.WriteLine($"Число {number} чётное");
 }
